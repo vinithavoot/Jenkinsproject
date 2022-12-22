e9feb0858b44535eb5a3842d5d736b138cb0ad50
Ponnambalam <vinitha.ponnambalam@accenture.com>
2022-12-22 13:52:39 +0530

    newly added line

--- src/com/jenkins/project/FirstClass.java
+++ src/com/jenkins/project/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi Vinitha, Welcome to Jenkins");
+		System.out.println("You are going to connect jenkins");
 	}
 
 }
